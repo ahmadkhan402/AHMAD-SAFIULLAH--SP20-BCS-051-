2f1692c9013ee62dd96d9a05956a4abc106af994
ahmadkhan402 <111109170+ahmadkhan402@users.noreply.github.com>
2022-12-19 21:06:41 +0500

    Update HomeScreen.js

--- Assgnment 3/HomeScreen.js
+++ Assgnment 3/HomeScreen.js
@@ -1,20 +1,37 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = (props) => {
+  const [Username, setUsername] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
   // const {getusername} = props;
-  // useEffect(()=>{
-  //   getUsername();
-  // },[])
-  // const getUsername = async()=>{
-  // const username = await AsyncStorage.getItem('USERNAME');
-  // }
+  useEffect(() => {
+    getUsername();
+  })
+  const getUsername = async () => {
+    const username = await AsyncStorage.getItem('USERNAME');
+    const email = await AsyncStorage.getItem('EMAIL');
+    const password = await AsyncStorage.getItem('PASSWORD');
+    setUsername(username);
+    setEmail(email);
+    setPassword(password);
+    
+
+  }
   return (
     <View style={styles.container}>
-   
-      <Text style={styles.text}>Welcome To Profile Screen </Text>
+      <View style={styles.headerConatiner}>
+        <Text style={styles.text}>Welcome To Profile Screen</Text>
+        <View style={styles.ContentContainer}>
+          <Text style={styles.username}>UserName:  <Text style={{fontSize:19,fontFamily:"italic",fontWeight:"100"}}>{Username}</Text></Text>
+          <Text style={styles.Email}>Email: <Text style={{fontSize:19,fontFamily:"italic",fontWeight:"100"}}>{Email}</Text> </Text>
+          <Text style={styles.Password}>Password: <Text style={{fontSize:19,fontFamily:"italic",fontWeight:"100"}}>{Password}</Text> </Text>
 
+        </View>
+      </View>
     </View>
   )
 }
@@ -22,16 +39,77 @@ const HomeScreen = (props) => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:"grey"
-    },
-    text:{
-      fontSize:30,
-      color:"red",
-      fontWeight:"700",
-      alignContent:"center"
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "grey"
+  },
+  headerConatiner: {
+    width: "90%",
+    height: "80%",
+    backgroundColor: "white",
+    borderRadius: 30,
+
+
+  },
+  text: {
+    marginTop: 15,
+    fontSize: 20,
+    padding: 5,
+    width: "80%",
+    alignSelf: "center",
+    textAlign: "center",
+    backgroundColor: "#FF1493",
+    alignSelf: "center",
+    color: "white",
+    fontWeight: "700",
+    marginBottom: 40,
+    borderRadius: 10,
+
+
+  },
+  ContentContainer: {
+   
+    width: "80%",
+    height: "70%",
+    alignSelf:"center",
+   marginTop:20,
+   borderRadius:20,
+    backgroundColor: "lightblue",
+    paddingTop:20,
+  },
+  username:{
+    fontSize:19,
+    fontWeight:"bold",
+    width:"90%",
+    
+    backgroundColor:"pink",
+    margin:3,
+    alignItems:"center",
+    alignSelf:"center",
+    borderRadius:6,
+  },
+  Email:{
+    fontSize:19,
+    fontWeight:"bold",
+    width:"90%",
+    
+    backgroundColor:"pink",
+    margin:3,
+    alignItems:"center",
+    alignSelf:"center",
+    borderRadius:6,
+  },
+  Password:{
+    fontSize:19,
+    fontWeight:"bold",
+    width:"90%",
+    
+    backgroundColor:"pink",
+    margin:3,
+    alignItems:"center",
+    alignSelf:"center",
+    borderRadius:6,
+  }
 })
